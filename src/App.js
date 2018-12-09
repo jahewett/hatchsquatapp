012899b85da6c15251815cb6033f87b0d7e8c6b1
James Hewett <jhewett@sonovate.com>
2018-12-09 21:38:20 +0000

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NavBar from './NavBar/NavBar';
 import logo from './logo_transparent.png';
 import './App.css';
 
@@ -6,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="functions(&fitness);" />
         </header>
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const drawerWidth = 300;
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: '#282c34',
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+function PermanentDrawerLeft(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Functions(&Fitness);
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+            <ListItem button key="Profile">
+              <ListItemIcon class="fas fa-user-alt"></ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItem>
+            <ListItem button key="Hatch Squat Calculator">
+              <ListItemIcon class="fas fa-calculator"></ListItemIcon>
+              <ListItemText primary="Hatch Squat Calculator" />
+            </ListItem>
+            <ListItem button key="Contact">
+              <ListItemIcon class="fas fa-envelope"></ListItemIcon>
+              <ListItemText primary="Contact" />
+            </ListItem>
+        </List>
+      </Drawer>
+    </div>
+  );
+}
+
+PermanentDrawerLeft.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(PermanentDrawerLeft);
